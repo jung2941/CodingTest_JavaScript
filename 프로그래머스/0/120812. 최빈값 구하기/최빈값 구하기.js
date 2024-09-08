3cd1a324aab3fd6322dd757546697d805e7d9e2f
Yujin Jung <154859664+jung2941@users.noreply.github.com>
2024-09-09 01:23:27 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,10 +1,6 @@
 function solution(array) {
-
-    let setArr = [...new Set(array)];
-
-    let calArr = setArr.map((v,i) => array.filter(dv => dv == v).length);
-
-    let rsltArr = calArr.map((mv,mi) => { if(mv == Math.max(...calArr)) return mi; return -1}).filter(v => v != -1)
-
-    return rsltArr.length > 1 ? -1 : setArr[rsltArr[0]];
+    let len = (array.length);
+    let num = array.map(arrays => len);
+    var answer = 0; 
+    return answer;
 }
