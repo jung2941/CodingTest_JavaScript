279637e4a29830cd46aa040dc4c2e65431f0defc
Yujin Jung <154859664+jung2941@users.noreply.github.com>
2024-10-05 23:48:17 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    let evenCount = 0;
+    let oddCount = 0;
+
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2 === 0) {
+            evenCount++;
+        } else {
+            oddCount++; 
+        }
+    }
+
+    var answer = [evenCount, oddCount];
+    return answer;
+}
